ba0faf6eead312b17b79f3e6e9dcee507ae7ca3d
Diego Inácio <diegodci@gmail.com>
2022-07-09 09:22:49 -0300

    Update notebook template - Get info from custom meta tags

--- assets/template/notebook.js
+++ assets/template/notebook.js
@@ -1,7 +1,29 @@
-// ! Navbar
-// * Buttons
-const NAVBAR_SL = ".notebook-navbar";
+// prettier-ignore
+// ! Declare global variables
+// ! and read custom meta tags
+{
+  var REPO_NAME = "Machine Learning Notebooks";
+  var REPO_ID = "machine-learning-notebooks";
+  var PAGE_ID = `${REPO_ID}-page`;
+  var SECTION_NAME = document.querySelector("meta[name='section_name']").content;
+  var SECTION_ID = document.querySelector("meta[name='section_id']").content;
+  var NOTEBOOK_NAME = document.querySelector("meta[name='notebook_name']").content;
+  var NOTEBOOK_ID = document.querySelector("meta[name='notebook_id']").content;
+}
 
+// prettier-ignore
+// ! Build URLs
+{
+  var WEBSITE_URL = "https://diegoinacio.github.io";
+  var GITHUB_URL = "https://github.com/diegoinacio";
+  var PN_URL = `${WEBSITE_URL}/python-notebooks/`;
+  var INDEX_PAGE_URL = `${WEBSITE_URL}/${PAGE_ID}/`;
+  var REPO_URL = `${GITHUB_URL}/${REPO_ID}`;
+  var NOTEBOOK_URL = `${REPO_URL}/blob/master/${SECTION_ID}/${NOTEBOOK_ID}.ipynb`;
+}
+
+// ! Navbar buttons
+const NAVBAR_SL = ".notebook-navbar";
 const div_buttons = document.createElement("div");
 div_buttons.className = "navbar-buttons";
 document.querySelector(NAVBAR_SL).appendChild(div_buttons);
@@ -10,20 +32,13 @@ let a_button;
 
 // * View Notebooks
 a_button = document.createElement("a");
-
-// ? Find the link of the notebook in the first cell
-const first_cell = document.querySelector("h1").parentNode;
-const notebook_link = first_cell.querySelector("a[href*=\\.ipynb]");
-
-if (notebook_link) {
-  a_button.href = notebook_link.href;
-  a_button.innerHTML = `
+a_button.href = NOTEBOOK_URL;
+a_button.innerHTML = `
   <i class="fas fa-book-open"></i>
   <span class="tooltip">
     View <b>Notebook</b>
   </span>`;
-  div_buttons.appendChild(a_button);
-}
+div_buttons.appendChild(a_button);
 
 // * Report an Issue
 // ? URL Encoding for Special Characters
@@ -36,27 +51,22 @@ const URL_ENCODING = {
 };
 
 // ? Get notebook title and section
-let [NB_TITLE, NB_SECTION] = document.head
-  .querySelector("title")
-  .innerText.split(" | ");
-
-NB_TITLE = NB_TITLE.split("");
-NB_TITLE = NB_TITLE.map((e) =>
+let NB_NAME = NOTEBOOK_NAME.split("");
+NB_NAME = NB_NAME.map((e) =>
   Object.keys(URL_ENCODING).includes(e) ? URL_ENCODING[e] : e
 );
-NB_TITLE = NB_TITLE.join("");
+NB_NAME = NB_NAME.join("");
 
-NB_SECTION = NB_SECTION.split("");
-NB_SECTION = NB_SECTION.map((e) =>
+let SC_NAME = SECTION_NAME.split("");
+SC_NAME = SC_NAME.map((e) =>
   Object.keys(URL_ENCODING).includes(e) ? URL_ENCODING[e] : e
 );
-NB_SECTION = NB_SECTION.join("");
+SC_NAME = SC_NAME.join("");
 
 // ? Build url to issue page
-let ISSUE_URL =
-  "https://github.com/diegoinacio/machine-learning-notebooks/issues/new";
-ISSUE_URL += `?labels=${NB_SECTION},from+page`;
-ISSUE_URL += `&title=[${NB_TITLE}]+Issue+title..`;
+let ISSUE_URL = `${REPO_URL}/issues/new`;
+ISSUE_URL += `?labels=${SC_NAME},from+page`;
+ISSUE_URL += `&title=[${NB_NAME}]+Issue+title..`;
 ISSUE_URL += "&body=Put+your+comment+here..";
 
 a_button = document.createElement("a");
@@ -69,20 +79,19 @@ a_button.innerHTML = `
   </span>`;
 div_buttons.appendChild(a_button);
 
-// * Return to Machine Learning Notebooks
+// * Return to index page
 a_button = document.createElement("a");
-a_button.href =
-  "https://diegoinacio.github.io/machine-learning-notebooks-page/";
+a_button.href = INDEX_PAGE_URL;
 a_button.innerHTML = `
-  <i class="fas fa-robot"></i>
+  <i class="fas fa-laptop-code"></i>
   <span class="tooltip">
-    Return to <b>Machine Learning Notebooks</b>
+    Return to <b>${REPO_NAME}</b>
   </span>`;
 div_buttons.appendChild(a_button);
 
 // * Go to Python Notebooks
 a_button = document.createElement("a");
-a_button.href = "https://diegoinacio.github.io/python-notebooks/";
+a_button.href = PN_URL;
 a_button.innerHTML = `
   <i class="fab fa-python"></i>
   <span class="tooltip">
@@ -92,7 +101,7 @@ div_buttons.appendChild(a_button);
 
 // * Visit my personal website
 a_button = document.createElement("a");
-a_button.href = "https://diegoinacio.github.io/";
+a_button.href = WEBSITE_URL;
 a_button.innerHTML = `
   <i class="fas fa-home"></i>
   <span class="tooltip">
@@ -126,7 +135,7 @@ let year_ft = new Date().getFullYear();
 let content_footer = `
 <hr>
 <p>© ${year_ft} Diego Inácio.</p>
-<a href="https://diegoinacio.github.io/" target="_blank">diegoinacio.github.io</a>
+<a href="${WEBSITE_URL}" target="_blank">diegoinacio.github.io</a>
 `;
 
 document.querySelector(FOOTER_SL).innerHTML = content_footer;
